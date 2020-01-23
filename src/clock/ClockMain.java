9616cf520906def1e0873546aa6d8e0bfa61c2a2
AZAEL <AZAEL@DESKTOP-Q8MDA1A>
2020-01-23 18:29:01 +0000

    background image redo, and earky clock functions implemented

--- src/clock/ClockMain.java
+++ src/clock/ClockMain.java
@@ -1,5 +1,7 @@
 package clock;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.HeadlessException;
 import java.io.IOException;
 import javax.swing.ImageIcon;
@@ -8,23 +10,31 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ClockMain extends JFrame{
+    
+    private final ClockTimer clocktimer;
 
     public ClockMain() throws HeadlessException, IOException {
-        this.setLayout(null);
+        this.clocktimer= new ClockTimer();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.bgCharge();
+        this.setSize(1000, 1000);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        ClockHands clockHands = new ClockHands(clocktimer);
+        this.bgCharge();
+        this.add(clockHands);
     }
     
     private void bgCharge(){
-        JLabel background;
-        String backgroudClock ="/image/imgClock.jpg";
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon(getClass().getResource(backgroudClock)).getImage());
-        background=new JLabel("",imageIcon, JLabel.CENTER);
-        background.setBounds(0,0,imageIcon.getIconHeight(),imageIcon.getIconWidth());
-        add(background);
-        this.setSize(imageIcon.getIconHeight(), imageIcon.getIconWidth());
+        class Back extends JPanel{
+            @Override
+            public void paintComponent(Graphics g){
+                Dimension tam =getSize();
+                String backgroudClock ="/image/imgClock.jpg";
+                ImageIcon imageIcon = new ImageIcon(new ImageIcon(getClass().getResource(backgroudClock)).getImage());
+                g.drawImage(imageIcon.getImage(), 0, 0,tam.width, tam.height,null);
+            }       
+        }
+        this.add(new Back());
     }
 
     public static void main(String[] args) throws IOException {
